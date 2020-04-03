0b67c7d009a479f8a21280fc95270ceadcdacf1d
Swagger-Ranger <liufei32@outlook.com>
2020-04-03 17:15:34 +0800

    v1.0.4 excel批量导入表配置

--- modules/dataManager/metadata/script/backupTable.js
+++ modules/dataManager/metadata/script/backupTable.js
@@ -47,7 +47,8 @@ new Vue({
 	            { required: true, message: '数据平台对应表英文名', trigger: 'blur' }
 	          ],
 	        },
-	        formLabelWidth: '180px'
+	        formLabelWidth: '180px',
+			uploadWithToken: {token: token}
 	    }
     },
 	mounted() {
@@ -275,6 +276,38 @@ new Vue({
 					}
 				}
 			});
-	   	}
+	   	},
+		downloadTemplate(){
+			var url = baseURL + "downloadFile/template.xlsx";
+			console.log("------------------------------------" + baseURL);
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url, true);    // 也可以使用POST方式，根据接口
+			xhr.responseType = "blob";  // 返回类型blob
+			xhr.setRequestHeader('token',token);
+			// 定义请求完成的处理函数，请求前也可以增加加载框/禁用下载按钮逻辑
+			xhr.onload = function () {
+				// 请求完成
+				if (this.status === 200) {
+					// 返回200
+					var blob = this.response;
+					var reader = new FileReader();
+					reader.readAsDataURL(blob);  // 转换为base64，可以直接放入a表情href
+					reader.onload = function (e) {
+						// 转换完成，创建一个a标签用于下载
+						var a = document.createElement('a');
+						a.download = 'template.xlsx';
+						a.href = e.target.result;
+						$("body").append(a);  // 修复firefox中无法触发click
+						a.click();
+						$(a).remove();
+					}
+				}
+			};
+			// 发送ajax请求
+			xhr.send()
+		},
+		handleExeed(files){
+			this.$message.warning('当前限制选择 1 个文件，请重新选择');
+		}
   	}
 })
